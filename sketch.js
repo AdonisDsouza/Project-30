22a2adc4bb4a7812c77f908d4d428f158d2047e3
AdonisDsouza <72249060+AdonisDsouza@users.noreply.github.com>
2021-01-10 19:05:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,131 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var ground, chain, ball;
+var stand1, stand2;
+var box1, box2, box3, box4, box5, box6, box7, box8, box9, box10, box11, box12, box13, box14, box15, box16, box17, box18, box19, box20, box21, box22;
+var ground1, ground2, ground3;
+
+function preload() {
+  img = loadImage("hexagon (1).png");
+}
+
+function setup() {
+  createCanvas(1300, 400);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(750, 375, 1500, 50);
+  block1 = new Block(600,260,30,40);
+  block2 = new Block(570,260,30,40);
+  block3 = new Block(540,260,30,40);
+  block4 = new Block(630,260,30,40);
+  block5 = new Block(660,260,30,40);
+  block6 = new Block(585,220,30,40);
+  block7 = new Block(555,220,30,40);
+  block8 = new Block(615,220,30,40);
+  block9 = new Block(645,220,30,40);
+  block10 = new Block(600,170,30,40);
+  block11 = new Block(570,180,30,40);
+  block12 = new Block(630,180,30,40);
+  block13 = new Block(600,140,30,40);
+  block14 = new Block(900,170,30,40);
+  block15 = new Block(930,170,30,40);
+  block16 = new Block(870,170,30,40);
+  block17 = new Block(840,170,30,40);
+  block18 = new Block(960,170,30,40);
+  block19 = new Block(900,140,30,40);
+  block20 = new Block(930,140,30,40);
+  block21 = new Block(870,140,30,40);
+  block22 = new Block(900,110,30,40);
+  ground1 = new Ground(600,285,200,10);
+  ground2 = new Ground(900,195,200,10);
+  ground3 = new Ground(750, 600, 1500, 10)
+  player = new Player(50,200,30,30);
+  ball = Bodies.circle(50,200,20);
+  World.add(world,ball);
+  sling = new Chain(this.ball,{x:150, y:160});
+ 
+}
+
+function draw() {
+  background(0);
+  Engine.update(engine);
+
+  fill(rgb(135, 205, 236));
+
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+
+  fill("lightBlue");
+  block6.display();
+  block7.display();
+  block8.display();
+  block9.display();
+
+  fill("lightPink");
+  block10.display();
+  block11.display();
+  block12.display();
+  fill(rgb(47, 48, 48));
+
+  block13.display();
+
+  fill(rgb(135, 205, 236));
+  block14.display();
+  block15.display();
+  block16.display();
+  block17.display();
+  block18.display();
+  fill("lightGreen");
+
+  block19.display();
+  block20.display();
+  block21.display();
+  fill("lime");
+
+  block22.display();
+
+  ground1.display();
+  ground2.display();
+  ground3.display();
+
+  imageMode(CENTER);
+  image(img,ball.position.x,ball.position.y,40,40);
+
+  imageMode(CENTER);
+  image(img,ball.position.x,ball.position.y,40,40);
+
+  chain.display();
+
+  ball.display("darkblue", "orange");
+  textSize(35);
+  fill(0);
+  text("DRAG AND RELEASE THE BALL TO DESTROY THE BOXES", 150, 385)
+  fill("orange");
+  textSize(20);
+  text("PRESS SPACE FOR A SECOND CHANCE !!",20,20)
+}
+
+
+function mouseDragged(){
+  Matter.Body.setPosition(this.ball, {x: mouseX , y: mouseY});
+}
+
+
+function mouseReleased(){
+  sling.fly();
+}
+
+function keyPressed(){
+	if(keyCode === 32)
+	{
+		Matter.Body.setPosition(player.body,{x:235, y:420})
+		chain.attach(player.body);
+	}
+}
